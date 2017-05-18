3808cbc7d3a6c96d7e4a581b084bc89fffad3ef7
Yuqiao Jiang <yujiang@ucsc.edu>
2017-05-17 21:55:09 -0700

    preloader

--- Rhinoceromance/js/States/preloader.js
+++ Rhinoceromance/js/States/preloader.js
@@ -2,24 +2,11 @@
 var Preloader={
 	preload: function() { // preload assets
 		console.log('Preloader: preload');
-<<<<<<< HEAD
-		this.load.atlas("buttonsheet","assets/img/buttonsheet.png","assets/img/buttonsheet.json");
-		this.load.atlas("nextbutton","assets/img/nextbutton.png","assets/img/nextbutton.json");
-		this.load.image("sudan","assets/img/sudan.png");
-		this.load.image("heart","assets/img/heart.png");
-		this.load.image("brokenheart","assets/img/broken_heart.png");
-		this.load.image("apartment","assets/img/bg.png");
-		this.load.image("base","assets/img/base.png");
-		this.load.image("exercising","assets/img/exercise.png");
-		this.load.image("resting","assets/img/rest.png");
-		this.load.image("studying","assets/img/study.png");
-		this.load.image("stylin","assets/img/stylin.png");
-		this.load.image("statwindow","assets/img/statwindow.png");
-		this.load.image("daytimer","assets/img/daytimer.png");
-=======
+
 		this.load.path = 'assets/';
 		
 		this.load.atlas("buttonsheet","img/buttonsheet.png","img/buttonsheet.json");
+		this.load.atlas("nextbutton","img/nextbutton.png","img/nextbutton.json");
 		this.load.image("sudan","img/sudan.png");
 		this.load.image("heart","img/heart.png");
 		this.load.image("brokenheart","img/broken_heart.png");
@@ -27,11 +14,15 @@ var Preloader={
 		this.load.image("base","img/base.png");
 		this.load.image("statwindow","img/statwindow.png");
 		this.load.image("daytimer","img/daytimer.png");
+		this.load.image("exercising","img/exercise.png");
+		this.load.image("resting","img/rest.png");
+		this.load.image("studying","img/study.png");
+		this.load.image("stylin","img/stylin.png");
 
 		this.load.audio("keypress", "audio/key.wav");
 
 		this.load.bitmapFont('blackFont','font/black.png', 'font/black.fnt');
->>>>>>> 33c673bcf865b710aeea6f7ba5299e5bb1c80806
+
 	},
 	create: function() {
 		console.log('Preloader: create');
