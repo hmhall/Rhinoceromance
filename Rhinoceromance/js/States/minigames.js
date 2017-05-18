a78f8b3badb4937089d1bdb0bd28b0153ad66a02
Hutson Hall <hmhall@ucsc.edu>
2017-05-17 22:34:32 -0700

    stats now change upon completing minigame

--- Rhinoceromance/js/States/minigames.js
+++ Rhinoceromance/js/States/minigames.js
@@ -6,21 +6,32 @@ var MiniGames={
 		switch(actiontaken){
 			case 1:
 				this.add.text(0, 50,"This button represents the mini\n game that will correspond to the Charm test",{fontSize: "32px", fill: "white" });
+				charm++;
+				stress++;
 				break;
 			case 2:
 				this.add.text(0, 50,"This button represents the mini\n game that will correspond to the Fitness test",{fontSize: "32px", fill: "white" });
+				fitness++;
+				stress++;
 				break;
 			case 3:
 				this.add.text(0, 50,"This button represents the mini\n game that will correspond to the Smarts test",{fontSize: "32px", fill: "white" });
+				smarts++;
+				stress++;
 				break;
 			case 4:
 				this.add.text(0, 50,"This button represents the mini\n game that will correspond to the Style test",{fontSize: "32px", fill: "white" });
+				style++;
+				stress++;
 				break;
 			case 5:
 				this.add.text(0, 50,"This button represents the mini\n game that will correspond to the Stress test",{fontSize: "32px", fill: "white" });
+				stress--;
 				break;
 			case 6:
 				this.add.text(0, 50,"This button represents the mini\n game that will correspond to the Money test",{fontSize: "32px", fill: "white" });
+				money++;
+				stress+=2;
 				break;
 		}
 	},
