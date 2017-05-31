d43380a66828c1701ba20c23e7dd33706c2929c5
Hutson Hall <hmhall@ucsc.edu>
2017-05-30 21:10:02 -0700

    commiting so I can pull

--- Rhinoceromance/js/States/datenight.js
+++ Rhinoceromance/js/States/datenight.js
@@ -4,11 +4,20 @@ var DateNight={
 		console.log("DateNight: create");
 		var cannery = game.add.sprite(0,0,"cannery");
 		this.textarray=[];
-		this.textindex=0;
 		this.readInText();
-		this.datetext=game.add.text(25, 650, "Thank you for playing our first build!", {fontSize: "32px", fill: "black"});
 
-		game.input.onDown.add(this.advancetext,this);
+		var titletY = new Typewriter();
+		titletY.init(Mainmenu, {
+			x: 100,
+			y: 500,
+			fontFamily: "blackFont",
+			fontSize: 32,
+			maxWidth: 800,
+			sound: this.keypress,
+			dialogues: this.textarray,
+			dialogueEndFn: this.changeState
+		});
+		titletY.start();
 	},
 	update: function(){
 		
@@ -17,16 +26,12 @@ var DateNight={
 		console.log("DateNight: resolveDate "+dateChoice);
 		
 	},
-	advancetext:function(){
-		console.log("DateNight: advancetext")
-		this.datetext.text=this.textarray[this.textindex];
-		this.textindex++;
-	},
-	readInText:function(){ //temporary function until I talk to Matt about reading in text
+	readInText:function(){ //temporary function until we figure out reading in text
 		this.textarray[0]="according to all known laws of aviation";
 		this.textarray[1]="there is no way a bee should be able to fly";
 		this.textarray[2]="its wings are too small to get its fat little body off the ground";
 		this.textarray[3]="the bee, of course, flies anyway";
 		this.textarray[4]="beacause bees don't care what humans think is impossible";
+		
 	}
 };
